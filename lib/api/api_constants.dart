cfee057f85c44fde331eb89e337584cbc8245dec
Blue Sky <stylistdire68@gmail.com>
2025-01-26 15:58:09 -0500

    add some provider

--- lib/api/api_constants.dart
+++ lib/api/api_constants.dart
@@ -1,3 +1,4 @@
 class ApiConstants {
-  static const baseUrl = 'https://singularly-hot-cod.ngrok-free.app/api/v1';
+  static const baseUrl =
+      'https://rapidly-probable-halibut.ngrok-free.app/api/v1';
 }
